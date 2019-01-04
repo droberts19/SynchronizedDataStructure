be2d1c8b0bc3c756a73bbb69c56169c58c6ad18f
droberts19 <droberts19@scienceleadership.org>
2019-01-04 13:32:12 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class Controller implements Runnable {
+    private int z = 1;
+    private int a = 0;
+
+
+    private void doThings() {
+        System.out.println();
+        System.out.println("Count up to 100 and back down to 0");
+        for (int i = 0; i < 200; i++) {
+            if (z == 1) {
+                a = a + 1;
+                System.out.println(a);
+            }
+            if (z == 2) {
+                a = a - 1;
+                System.out.println(a);
+            }
+            if (a == 100) {
+                z = 2;
+            }
+        }
+    }
+
+    public void run() {
+        doThings();
+    }
+}
